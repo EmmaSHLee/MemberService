69f198696e92a2218595a5dd3dafadc19ad68c1a
EMMA <2371045@ewhain.net>
2024-11-23 14:14:18 +0900

    fourth commit

--- src/main/java/com/example/business/SpringConfig.java
+++ src/main/java/com/example/business/SpringConfig.java
@@ -1,13 +1,24 @@
 package com.example.business;
 
+import com.example.business.repository.JdbcMemberRepository;
 import com.example.business.repository.MemberRepository;
 import com.example.business.repository.MemoryMemberRepository;
 import com.example.business.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig {
+
+    private DataSource dataSource;
+
+    @Autowired
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
 
     @Bean
     public MemberService memberService() {
@@ -16,6 +27,7 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+        //return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
     }
 }
--- src/main/java/com/example/business/controller/MemberController.java
+++ src/main/java/com/example/business/controller/MemberController.java
@@ -1,8 +1,14 @@
 package com.example.business.controller;
 
+import com.example.business.domain.Member;
 import com.example.business.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.*;
 
 @Controller
 public class MemberController {
@@ -11,5 +17,26 @@ public class MemberController {
     @Autowired //MemberController 생성될 때 등록되어 있는 MemberService 객체를 가져옴
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
+    }
+
+    @GetMapping("/members/new")
+    public String createForm(){
+        return "members/createMemberForm";
+    }
+    @PostMapping("/members/new")
+    public String create(MemberForm form) {
+        Member member = new Member();
+        member.setName(form.getName());
+
+        memberService.join(member);
+
+        return "redirect:/";
+    }
+
+    @GetMapping("/members")
+    public String list(Model model) {
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+        return "members/memberList";
     }
 }
